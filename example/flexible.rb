d509402b2b1dfc7749ab890302d9b863ce357c97
Kenichi Kamiya <kachick1@gmail.com>
2012-10-04 05:27:43 +0900

    .auto for auto printable in another thread

---
+++ example/flexible.rb
@@ -0,0 +1,27 @@
+$VERBOSE = true
+
+require_relative '../lib/terminal/progressbar'
+
+Terminal::ProgressBar.run mark: '*' do |bar|
+  50.times do
+    sleep 0.1
+    bar.increment!
+  end
+  bar.pointer = 15
+  bar.flush
+  sleep 2
+
+  30.times do
+    sleep 0.1
+    bar.increment! 2
+  end
+
+  30.times do
+    sleep 0.1
+    bar.decrement! 2
+  end
+
+  bar.pointer = 70
+  bar.flush
+  sleep 2
+end
